9cdb333b456db0d1c90c6e3eeb3eb457ecd104e0
mikematar1 <mike.matar97@gmail.com>
2023-12-20 23:55:35 +0200

    major changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: '', component: MovieListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../movie.service';
 import{Movie} from '../movie-card/movie-card.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,11 +11,50 @@ import{Movie} from '../movie-card/movie-card.model';
 export class MovieListComponent implements OnInit {
   movies: Movie[] = [];
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService,private router:Router,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.movieService.getPopularMovies().subscribe((data: any) => {
-      this.movies = data.results;
+
+    this.route.url.subscribe(urlSegments => {
+      if (urlSegments.length) {
+        const path = urlSegments[0].path;
+        console.log(path);
+        switch (path) {
+          case 'now-playing':
+            //disable the next and previous buttons
+
+            break;
+          case 'popular':
+
+            break;
+          case 'top-rated':
+
+            break;
+          case 'upcoming':
+
+            break;
+          case 'favorites':
+
+            break;
+          case 'genre':
+
+            break;
+          default:
+
+            //for searching
+            break;
+        }
+      }else{
+        //normal movies
+        this.movieService.getMovies().subscribe((data: any) => {
+          this.movies = data.results;
+        });
+      }
     });
+
+
+  }
+  navigateToMovieDetails(movieId:any){
+    this.router.navigate(['movie',movieId]);
   }
 }
